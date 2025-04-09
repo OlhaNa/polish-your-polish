d69a9e0487afff4b279ae07064e23cb0bd1ac469
OlyaKnyzhna <olha.nadvodna@gmail.com>
2025-04-09 20:54:59 +0100

    add live data from google sheet

--- src/data/WordData.ts
+++ src/data/WordData.ts
@@ -1,43 +1,33 @@
+import Papa from "papaparse";
 import Dictionary from "../types/Dictionary";
 
-const dictionary: Dictionary = {
-  time: {
-    days: [
-      {
-        english: "Monday",
-        polish: "poniedziałek",
-      },
-      {
-        english: "Tuesday",
-        polish: "wtorek",
-      },
-      {
-        english: "Wednesday",
-        polish: "środa",
-      },
-      {
-        english: "Thursday",
-        polish: "czwartek",
-      },
-      {
-        english: "Friday",
-        polish: "piątek",
-      },
-      {
-        english: "Saturday",
-        polish: "sobota",
-      },
-      {
-        english: "Sunday",
-        polish: "niedziela",
-      },
-    ],
-  },
-};
+interface DataRow {
+  topic: string;
+  subtopic: string;
+  english: string;
+  polish: string;
+}
+
+const sheetUrl =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTmM7H1tdP4zJWOnXXOpFX4whJUrzvFZZLQ-asJ6kqdodG9hQTVfooKfNXGKNd67xnYPGBVAIe-HdNR/pub?gid=0&single=true&output=csv";
 
 export const getDictionary = async () => {
-  await new Promise((resolve) => {
-    setTimeout(resolve, 2000);
+  const response = await fetch(sheetUrl);
+  const data = await response.text();
+  const dataRows = Papa.parse<DataRow>(data, { header: true }).data;
+  const dictionary: Dictionary = {};
+  dataRows.forEach((dataRow) => {
+    if (!(dataRow.topic in dictionary)) {
+      dictionary[dataRow.topic] = {};
+    }
+    if (!(dataRow.subtopic in dictionary[dataRow.topic])) {
+      dictionary[dataRow.topic][dataRow.subtopic] = [];
+    }
+    dictionary[dataRow.topic][dataRow.subtopic].push({
+      english: dataRow.english,
+      polish: dataRow.polish,
+    });
   });
+
   return dictionary;
 };
